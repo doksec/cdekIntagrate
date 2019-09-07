1773fcc3e6e9c1fa328cee114162324b69e31b09
Pavel Zarubin <pavel@orendat.ru>
2019-09-07 14:59:48 +0300

    Add new field in modx map

--- core/components/cdekintgrate/model/cdekintgrate.class.php
+++ core/components/cdekintgrate/model/cdekintgrate.class.php
@@ -239,7 +239,66 @@ class cdekIntgrate
         $address = $order->getOne('Address');
         $products = $order->getMany('Products');
 
+        $order = new Common\Order([
+            'Number'   => 'TEST-123456',
+            'SendCityCode'    => 44, // Москва
+            'RecCityPostCode' => '630001', // Новосибирск
+            'RecipientName'  => 'Иван Петров',
+            'RecipientEmail' => 'petrov@test.ru',
+            'Phone'          => '+7 (383) 202-22-50',
+            'TariffTypeCode' => 139, // Посылка дверь-дверь от ИМ
+        ]);
 
+        $order->setAddress(Common\Address::create([
+            'Street' => 'Холодильная улица',
+            'House'  => '16',
+            'Flat'   => '22',
+        ]));
+
+        $package = Common\Package::create([
+            'Number'  => 'TEST-123456',
+            'BarCode' => 'TEST-123456',
+            'Weight'  => 500, // Общий вес (в граммах)
+            'SizeA'   => 10, // Длина (в сантиметрах), в пределах от 1 до 1500
+            'SizeB'   => 10,
+            'SizeC'   => 10,
+        ]);
+
+        $package->addItem(new Common\Item([
+            'WareKey' => 'NN0001', // Идентификатор/артикул товара/вложения
+            'Cost'    => 500, // Объявленная стоимость товара (за единицу товара)
+            'Payment' => 0, // Оплата за товар при получении (за единицу товара)
+            'Weight'  => 120, // Вес (за единицу товара, в граммах)
+            'Amount'  => 2, // Количество единиц одноименного товара (в штуках)
+            'Comment' => 'Test item',
+        ]));
+
+        $order->addPackage($package);
+
+        $request = new Requests\DeliveryRequest([
+            'Number' => 'TESTING123',
+        ]);
+        $request->addOrder($order);
+
+        $response = $client->sendDeliveryRequest($request);
+
+        if ($response->hasErrors()) {
+            // обработка ошибок
+
+            foreach ($response->getErrors() as $order) {
+                // заказы с ошибками
+                $order->getMessage();
+                $order->getErrorCode();
+                $order->getNumber();
+            }
+        }
+
+        foreach ($response->getOrders() as $order) {
+            // сверяем данные заказа, записываем номер
+            $order->getNumber();
+            $order->getDispatchNumber();
+            break;
+        }
         return 123;
     }
 
--- core/components/cdekintgrate/ms2/msOrderAddress.inc.php
+++ core/components/cdekintgrate/ms2/msOrderAddress.inc.php
@@ -2,9 +2,16 @@
 return [
     'fields' => [
         'cdek_id' => NULL,
+        'inner_cdek_id' => NULL,
     ],
     'fieldMeta' => [
         'cdek_id' => [
+            'dbtype' => 'int',
+            'precision' => '10',
+            'phptype' => 'integer',
+            'null' => true,
+        ],
+        'inner_cdek_id' => [
             'dbtype' => 'int',
             'precision' => '10',
             'phptype' => 'integer',
